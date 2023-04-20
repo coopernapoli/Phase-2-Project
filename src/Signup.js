5992437e4d14ff0b002deb87ab0f427d18beb0a5
Cooper Napoli <napolisc10@gmail.com>
2023-04-19 18:10:00 -0600

    Added gender, address, countries, phone, email, and username to Signup.js.

--- src/Signup.js
+++ src/Signup.js
@@ -16,7 +16,67 @@ const SignUp = () => {
                         <h2>Sign-Up</h2>
                         </div>
                         <div className="card-body">
-
+                            <div className="row">
+                                <div className="col-lg-6">
+                                    <div className="form-group">
+                                        <label>Username <span className="errmsg">*</span></label>
+                                        <input className="form-control"></input>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="form-group">
+                                        <label>Password<span className="errmsg">*</span></label>
+                                        <input type="password" className="form-control"></input>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="form-group">
+                                        <label>Email<span className="errmsg">*</span></label>
+                                        <input className="form-control"></input>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="form-group">
+                                        <label>Phone <span className="errmsg">*</span></label>
+                                        <input className="form-control"></input>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="form-group">
+                                        <label>Country<span className="errmsg">*</span></label>
+                                        <select className="form-control">
+                                            <option value="usa">USA</option>
+                                            <option value="uk">UK</option>
+                                            <option value="mexico">Mexico</option>
+                                            <option value="japan">Japan</option>
+                                            <option value="sweden">Sweden</option>
+                                            <option value="peru">Peru</option>
+                                            <option value="canada">Canada</option>
+                                            <option value="russia">Russia</option>
+                                            <option value="cuba">Cuba</option>
+                                        </select>
+                                    </div>
+                                <div className="col-lg-12">
+                                    <div className="form-group">
+                                        <label>Address</label>
+                                            <textarea className="form-control"></textarea>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="form-group">
+                                        <label>Gender</label>
+                                        <input type="radio" value="male" className="form-control"></input>
+                                        <label>Male</label>
+                                        <input type="radio" value="female" className="form-control"></input>
+                                        <label>Female</label>
+                                        <input type="radio" value="nonbinary" className="form-control"></input>
+                                        <label>Non-Binary</label>
+                                        <input type="radio" value="refuse" className="form-control"></input>
+                                        <label>Prefer Not To Answer</label>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
                         </div>
                         <div className="card-footer">
                             <button className="btn btn-primary" type="submit">Register</button>
